604d3e3acdc56281b400a4453b707908ec6fba0d
--global <misterj435@gmail.com>
2025-04-28 21:15:41 +0530

    portfolio init commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const scaleFactor = 4;
+export const dialogueData = {
+  pc: `This is my PC where I do most of my Ruby on Rails development — building apps, working with databases, and sometimes doing a bit of frontend work.`,
+
+  "cs-degree": `This is my Science degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table":  `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my TV. I often use it to watch programming tutorials, tech talks, and online courses to keep improving my skills.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1BNUdEcm6VIHb7yejUP39HIklOFqrqfkn/view?usp=drive_link" target="_blank">Check it out?</a>
+  Contact me at hifzan734@gmail.com if you have any interesting job opportunities!`,
+  projects: `You can find my projects on GitHub: <a href="https://github.com/mohammad-hifzan" target="_blank">mohammad-hifzan</a>. I regularly work on web applications, tools, and experiments to improve my skills.`,
+  library: `These are some of my certificates displayed in my library. They reflect the courses and skills I've completed over time. You can view more details here: <a href="https://www.freecodecamp.org/Hifzan" target="_blank">click here</a>, and also check out my LinkedIn profile for my complete professional background: <a href="https://www.linkedin.com/in/mohammad-hifzan-9a879b231" target="_blank">Hifzan</a>.`,
+};
